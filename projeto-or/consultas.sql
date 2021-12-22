839170bf3428354a09c57df955be82e3ec1db065
Douglas Ventura <douglasventura.pe@gmail.com>
2021-12-22 03:24:57 -0300

    adicionado algumas consultas

--- projeto-or/consultas.sql
+++ projeto-or/consultas.sql
@@ -1,29 +1,23 @@
-SELECT VALUE(R).nome, cpf,salario FROM tb_entregador R;
-
-SELECT telefones
-FROM tb_cliente
-WHERE cpf='4124223412';
-
---seleciona cpf de cliente e entregador onde possuem os mesmo nomes
-SELECT c.cpf, v.cpf 
-FROM tb_cliente c 
-    LEFT JOIN tb_entregador v
-        ON c.nome = v.nome;
-
 --seleciona o id e tipo do frete onde preco do frete é maior que 10
-SELECT pr.id, pr.tipo
-FROM tb_frete pr
-WHERE pr.preco = ANY (SELECT pr.preco FROM tb_frete pr WHERE pr.preco > 10);
+SELECT f.tipo, f.preco
+FROM tb_frete f
+WHERE f.preco = ANY (SELECT f.preco FROM tb_frete f WHERE f.preco > 50);
 
 --preco do frete maior media preco
-SELECT av.id, av.tipo
-FROM tb_frete av
-WHERE av.preco > (SELECT AVG(pr.preco) FROM tb_frete pr);
+SELECT f.id, f.tipo, f.preco
+FROM tb_frete f
+WHERE f.preco > (SELECT AVG(pr.preco) FROM tb_frete pr);
 
 -- CONSULTANDO OS NUMS DE TELEFONE DO CLIENTE DE CPF '520...'
 SELECT *
 FROM TABLE(
     SELECT c.telefones
     FROM tb_cliente c
-    WHERE cpf = '520.644.719-60'
+    WHERE cpf = '859.839.606-01'
 );
+
+-- CONSULTANDO O SUPERVISOR DE UM FUNCIONARIO
+SELECT e.supervisor.nome
+FROM tb_entregador e
+WHERE e.nome = 'Leandro Campos';
+
