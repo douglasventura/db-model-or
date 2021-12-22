56b120c60d03b6fa81ccec5cccaff4dad8e807a1
Douglas Ventura <douglasventura.pe@gmail.com>
2021-12-22 05:14:28 -0300

    mais consultas adicionadas

--- projeto-or/consultas.sql
+++ projeto-or/consultas.sql
@@ -8,7 +8,7 @@ SELECT f.id, f.tipo, f.preco
 FROM tb_frete f
 WHERE f.preco > (SELECT AVG(pr.preco) FROM tb_frete pr);
 
--- CONSULTANDO OS NUMS DE TELEFONE DO CLIENTE DE CPF '520...'
+-- CONSULTANDO OS NUMS DE TELEFONE DO CLIENTE DE CPF '859...'
 SELECT *
 FROM TABLE(
     SELECT c.telefones
@@ -21,3 +21,30 @@ SELECT e.supervisor.nome
 FROM tb_entregador e
 WHERE e.nome = 'Leandro Campos';
 
+-- CONSULTANDO OS PACOTES DA CLIENTE 'LUANNA DIAS'
+SELECT e.cliente.nome, e.entregador.nome, e.pacote.status_entrega, e.pacote.id
+FROM tb_entrega e
+WHERE e.cliente.nome = 'Luanna Dias'
+
+-- CONSULTANDO OS PACOTES DA CLIENTE 'LUANNA DIAS' ENTREGUES
+SELECT e.cliente.nome, e.entregador.nome, e.pacote.status_entrega, e.pacote.id
+FROM tb_entrega e
+WHERE e.cliente.nome = 'Luanna Dias'
+AND e.pacote.status_entrega = 'ENTREGUE'
+
+-- CONSULTANDO OS SUPERVISORES DOS ENTREGADORES
+SELECT E.nome as NOME_DO_ENTREGADOR, DEREF(E.supervisor).nome as NOME_DO_SUPERVISOR
+FROM tb_entregador E;
+
+-- EXIBINDO O ENDERECO DA CLIENTE 'LUANNA DIAS' USANDO O PROCEDIMENTO DO TIPO PESSOA
+DECLARE
+    mb tp_cliente;
+BEGIN 
+    SELECT VALUE(c) INTO mb FROM tb_cliente c
+    WHERE c.nome = 'Luanna Dias';
+    mb.endereco.exibirEndereco();
+END;
+
+-- CONSULTAS DOS TELEFONES DE CADA CLIENTE
+SELECT d.nome, T.*
+FROM tb_cliente d, TABLE(d.telefones) T;
